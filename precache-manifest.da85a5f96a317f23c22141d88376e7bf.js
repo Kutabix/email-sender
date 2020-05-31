76e53b92a3d86b06df29cb4ceda03db8c0c265a6
Kutabix <bartekd123456@wp.pl>
2020-05-31 23:46:02 +0200

    Updates

---
+++ precache-manifest.da85a5f96a317f23c22141d88376e7bf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7db2012c6a925beff98182800b58d4a",
+    "url": "/email-sender/index.html"
+  },
+  {
+    "revision": "ec3897f23c5957bf6dc9",
+    "url": "/email-sender/static/css/main.e5fe45ed.chunk.css"
+  },
+  {
+    "revision": "88f4f23a4efb0ab54263",
+    "url": "/email-sender/static/js/2.0f20bff0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/email-sender/static/js/2.0f20bff0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec3897f23c5957bf6dc9",
+    "url": "/email-sender/static/js/main.e954ed0c.chunk.js"
+  },
+  {
+    "revision": "79dfc14a80ab2e8fbe48",
+    "url": "/email-sender/static/js/runtime-main.12812302.js"
+  },
+  {
+    "revision": "5371ea85044ba63951a3c0fe37deb185",
+    "url": "/email-sender/static/media/bg.5371ea85.jpg"
+  }
+]);
